194ee234349b6bba38653b080ef1c9e93f55ab99
Thor77 <xXThor77Xx@gmail.com>
2014-06-15 18:17:00 +0200

    edited pypi files

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 import os
 from setuptools import setup
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
 setup(
 
     name = 'TS3Py',
     version = '0.0.1',
     author = 'Thor77',
     author_email = 'thor77@xthor77.tk',
-    description = 'A python TS3Query-API',
+    description = 'A TS3Query-API',
     keywords = 'ts3 pyts3 ts3py',
     url = 'https://github.com/Thor77/TS3py',
-    long_description = read('README.md'),
-
+    long_description = 'A TS3Query-API. \r\n Look at the doc or the github-page for further information!',
 )
