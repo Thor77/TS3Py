4399c091621063a55b4ad57d25085cbbd71a14ff
Thor77 <xXThor77Xx@gmail.com>
2014-06-17 17:36:06 +0200

    fix in observer-plugin

--- plugins/channelobserve.py
+++ plugins/channelobserve.py
@@ -7,6 +7,7 @@ class Channelobserve(Plugin):
     def onLoad(self):
         self.stopFlag = Event()
         self.observer_thread = Observer(self.stopFlag, self.observe, 20)
+        self.observer_thread.start()
         self.blocklist = ['führer', 'konzentration', 'lager', '']
         # commands
 
@@ -15,7 +16,7 @@ class Channelobserve(Plugin):
         for cid in channellist:
             channelname = channellist[cid].lower()
             for string in self.blocklist:
-                if channelname.find(string.lower()) != 0:
+                if channelname.find(string.lower()) != -1:
                     self.bot.deleteChannel(cid)
 
     def onUnload(self):
