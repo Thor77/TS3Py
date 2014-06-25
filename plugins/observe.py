d63b12b7e4dd545c12e214a48b413d536a4c1516
Thor77 <xXThor77Xx@gmail.com>
2014-06-25 14:01:48 +0200

    created observer-plugin
    check every minute if there are channels with blocked name or topic and
    deletes them

---
+++ plugins/observe.py
@@ -0,0 +1,26 @@
+from plugin import Plugin
+from ts3bot import FuncThread
+
+class Observer(Plugin):
+
+    def onLoad(self):
+        self.funcThread = FuncThread(self.observe, 60)
+        self.funcThread.start() # start observer-thread
+        self.blocklist = ['insert', 'strings', 'here'] # blocklist
+        # commands
+
+    def observe(self):
+        print('observing...')
+        chanlist = self.bot.channellist()
+        print(chanlist)
+        for channel in chanlist:
+            cname = chanlist[channel]['channel_name'].lower()
+            if 'channel_topic' in chanlist[channel]:
+                ctopic = chanlist[channel]['channel_topic'].lower()
+            else:
+                ctopic = ''
+            for string in self.blocklist:
+                string = string.lower()
+                if cname.find(string) != -1 or ctopic.find(string) != -1:
+                    self.bot.channeldelete(channel)
+                    print('Channel %s deleted because "%s"!' % (cname, string))
