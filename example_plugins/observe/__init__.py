d1da676fd4b669bf601786ccf711d6b91637c0bc
Thor77 <xXThor77Xx@gmail.com>
2014-06-26 16:43:57 +0200

    improved observe-plugin
    blocked words in own file

--- example_plugins/observe/__init__.py
+++ example_plugins/observe/__init__.py
@@ -6,8 +6,10 @@ class Observer(Plugin):
     def onLoad(self):
         self.funcThread = FuncThread(self.observe, 60)
         self.funcThread.start() # start observer-thread
-        self.blocklist = ['insert', 'strings', 'here'] # blocklist
+        self.blocklist = []
+        self.readBlockFile()
         # commands
+        self.registerCommand('reloadBlockedWords', self.readBlockFile, 'Reload file with blocked words')
 
     def observe(self):
         print('observing...')
@@ -21,6 +23,12 @@ class Observer(Plugin):
             if any(string.lower() in cname or string.lower() in ctopic for string in self.blocklist):
                 self.bot.channeldelete(channel)
                 print('channel %s deleted because "%s"!' % (cname, string))
+
+    def readBlockFile(self):
+        filename = 'blocked.txt'
+        f = open(filename)
+        for line in f:
+            self.blocklist.append(line.strip())
 
     def onUnload(self):
         self.funcThread.stop()
