9a87aca7963b492ae57b5de8daa0c0ee83e218c5
Thor77 <xXThor77Xx@gmail.com>
2016-04-17 16:46:46 +0200

    update setup.py description, keywords and long_description

--- setup.py
+++ setup.py
@@ -4,15 +4,29 @@ from setuptools import setup
 setup(
 
     name='TS3Py',
-    version='0.3.1',
+    version='0.3.3',
     author='Thor77',
     author_email='thor77@thor77.org',
-    description='A TS3Query-API',
-    keywords='ts3 pyts3 ts3py',
+    description='A Python Teamspeak3-Query Library',
+    keywords='ts3 pyts3 ts3py teamspeak teamspeak3',
     url='https://github.com/Thor77/TS3py',
     long_description='''
-    A TS3Query-API.
-    Look at the doc or the github-page for further information!
+.. image:: https://travis-ci.org/Thor77/TS3Py.svg?branch=master
+    :target: https://travis-ci.org/Thor77/TS3Py
+
+.. image:: https://readthedocs.org/projects/ts3py/badge/?version=latest
+    :target: http://ts3py.readthedocs.org/en/latest/?badge=latest
+
+.. code-block:: python
+
+    from ts3py import TS3Query
+    c = TS3Query('ts.example.com')
+    c.login('serveradmin', 'password')
+    c.use(1)
+    channellist = c.channellist()
+    for channel_id in channellist:
+        print(channel_id, channellist[channel_id], sep='|')
+    c.disconnect()
     ''',
     py_modules=['ts3py', 'ts3utils'],
 
