7b86929792854cf1dfd1d9ef9a15ac0c40f907af
Thor77 <xXThor77Xx@gmail.com>
2016-04-22 20:20:55 +0200

    add response test-case

--- test_ts3utils.py
+++ test_ts3utils.py
@@ -14,6 +14,21 @@ command_data = {
                          ('reasonmsg', 'Go away!')]), [])
 }
 
+response_data = {
+    'error id=0 msg=ok': [
+        'error', OrderedDict([('id', 0), ('msg', 'ok')])
+    ],
+    'virtualserver_id=1 virtualserver_clientsonline=5\
+     virtualserver_uptime=2096023 virtualserver_name=Test\sServer1\
+     virtualserver_machine_id=testing|virtualserver_id=2': [
+        OrderedDict([('virtualserver_id', 1),
+                    ('virtualserver_clientsonline', 5),
+                    ('virtualserver_uptime', 2096023),
+                    ('virtualserver_name', 'Test Server1'),
+                    ('virtualserver_machine_id', 'testing')]),
+        OrderedDict([('virtualserver_id', 2)])],
+}
+
 
 def test_escape():
     for escaped, original in escape_data.items():
@@ -35,3 +50,8 @@ def test_unpack_command():
         unpacked = ts3utils.unpack_command(built)
         assert unpacked[0] == cmd
         assert unpacked[1] == params
+
+
+def test_unpack_response():
+    for response, parsed in response_data.items():
+        assert ts3utils.unpack_command(response) == parsed
