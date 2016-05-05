00dc98c9495e7724bba29736130e5f0d33f5f454
Thor77 <xXThor77Xx@gmail.com>
2016-05-05 16:26:30 +0200

    bump version to 0.4.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
 
     name='TS3Py',
-    version='0.4.0',
+    version='0.4.1',
     author='Thor77',
     author_email='thor77@thor77.org',
     description='A Python Teamspeak3-Query Library',
