059334cd78edce7e08f742c44c20055bc3913bce
Thor77 <xXThor77Xx@gmail.com>
2016-05-06 09:29:48 +0200

    make ts3py.objects easy accessible * import Channel, Client, Server, VirtualServer in ts3py/objects/__init__.py * import objects.Server in ts3py/__init__.py

---
+++ ts3py/__init__.py
@@ -0,0 +1,2 @@
+from ts3py.query import Query
+from ts3py.objects import Server
---
+++ ts3py/objects/__init__.py
@@ -0,0 +1,4 @@
+from ts3py.objects.client import Client
+from ts3py.objects.channel import Channel
+from ts3py.objects.virtual_server import VirtualServer
+from ts3py.objects.server import Server
