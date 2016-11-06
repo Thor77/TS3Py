2966ffe467ca29f6764d5687bd3750008121b12b
Thor77 <thor77@thor77.org>
2016-11-06 20:58:37 +0100

    Remove outdated package-description

--- setup.py
+++ setup.py
@@ -9,24 +9,6 @@ setup(
     description='A Python Teamspeak3-Query Library',
     keywords='ts3 pyts3 ts3py teamspeak teamspeak3',
     url='https://github.com/Thor77/TS3py',
-    long_description='''
-.. image:: https://travis-ci.org/Thor77/TS3Py.svg?branch=master
-    :target: https://travis-ci.org/Thor77/TS3Py
-
-.. image:: https://readthedocs.org/projects/ts3py/badge/?version=latest
-    :target: http://ts3py.readthedocs.org/en/latest/?badge=latest
-
-.. code-block:: python
-
-    from ts3py import TS3Query
-    c = TS3Query('ts.example.com')
-    c.login('serveradmin', 'password')
-    c.use(1)
-    channellist = c.channellist()
-    for channel_id in channellist:
-        print(channel_id, channellist[channel_id], sep='|')
-    c.disconnect()
-    ''',
     py_modules=['ts3py', 'ts3utils'],
 
 )
